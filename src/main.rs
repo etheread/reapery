d08dd3567614ac9c7b7456363894f5af06b782f6
etheread <provladychenko@gmail.com>
2026-09-26 13:34:14 +0300

    testing after renaming project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
+
+
+
 
 
 fn main() {
@@ -10,13 +15,67 @@ fn main() {
   "╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ",
     ];
 
+    println!("{}",LOGO.join("\n"));
+
+    let mut user_choice = String::new();
+
+    #[derive(Debug)]
+    enum Choice {
+        Transaction,
+        TrackWallet,
+        ContractData,
+        Exit,
+        Other
+    }
+
+    
+
+
     let choice:[&str;4] = [
-        "",
-        "",
-        "",
-        ""
+        "transaction",
+        "get wallet data",
+        "contract data",
+        "exit"
     ];
 
-    println!("{}",LOGO.join("\n"))
+    println!("Choose one option and write its number down:");
+    for (index,cho)    in choice.iter().enumerate() {
+        println!("{}.{}",index+1,cho);
+    }
+    println!("your number:");
+
+    
+    io::stdin().read_line(&mut user_choice).unwrap();
+
+   
+       let remember =  match user_choice.as_str().trim() {
+            "1" => Choice::Transaction,
+            "2" => Choice::TrackWallet,
+            "3" => Choice::ContractData,
+            "4" => Choice::Exit,
+            _ => Choice::Other
+        };
+                             
+            
+        match remember {
+            Choice::Transaction =>transaction(),
+            Choice::TrackWallet => get_wallet_data(),
+            Choice::ContractData => contract_data(),
+            Choice::Exit => println!("asd"),
+            Choice::Other => println!("try again") 
+        }
+            
+
+
+
 }
 
+fn get_wallet_data() {
+    println!("you chose track wallet function");
+}
+fn transaction() {
+    println!("you chose transaction function");
+}
+fn contract_data() {
+    println!("you chose contract data function");
+}
